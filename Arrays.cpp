7ba8f755f80fcc95f6f2a95b244fff104df02587
Priyansh Gupta <pg167783@gmail.com>
2023-01-14 02:02:19 +0530

    Array

--- Arrays.cpp
+++ Arrays.cpp
@@ -4,16 +4,31 @@ using namespace std;
 
 int main()
 {
-    int A[5];
-    A[0] = 10;
-    A[1] = 20;
-    A[2] = 30;
-    A[3] = 40;
-    A[4] = 50;
-    cout << sizeof(A) << endl; // size f one int is 4 bit so 4*5 = 20 bit
-    for (int i = 0; i < 5; i++)
+    int n;
+    cout << "Enter the size of array" << endl;
+    cin >> n;
+    int A[n];
+    for (int i = 0; i < n; i++)
     {
-        cout << A[i] << endl; // Transverse the array
+        cout << "Enter the value for index" << i << endl;
+        cin >> A[i];
+    }
+
+    // int A[5];
+    // A[0] = 10;
+    // A[1] = 20;
+    // A[2] = 30;
+    // A[3] = 40;
+    // A[4] = 50;
+    // cout << sizeof(A) << endl; // size f one int is 4 bit so 4*5 = 20 bit
+    // for (int i = 0; i < 5; i++)
+    // {
+    //     cout << A[i] << endl; // Transverse the array
+    // }
+
+    for (int x : A) // for each loop
+    {
+        cout << x << endl;
     }
 
     return 0;
